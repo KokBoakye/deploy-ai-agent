9dddff9cde998610a6ac185f3f12d37f5dd21db8
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2026-02-02 14:21:16 +0000

    deploy ai agent

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,13 +1,13 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-#   backend "s3" {
-#     bucket = "your-terraform-state-bucket"
-#     key    = "ai-agent/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "ai-agent-terraform-state-02-02-2026"
+    key    = "ai-agent/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
